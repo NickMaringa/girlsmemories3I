28d769989e5194cd31e20567485d41851a9d095d
NickMaringa <106190059+NickMaringa@users.noreply.github.com>
2024-09-02 08:44:01 -0300

    02/09

---
+++ entrevistas.js
@@ -0,0 +1,264 @@
+let entrevista1 = {
+    entrevistador: "Beatris",
+    entrevistado: "Nicole",
+    questoes: [
+        {
+        
+            p1: "Se você tivesse uma oportunidade de aconselhar sobre o futuro, para sua criança interior o que seria?"
+            r1: "."
+        },
+        {
+            p2: "Você acha que a imagem que você tem sobre você é a mesma que as outras pessoas tem?"
+            r2: "." 
+        },
+        {
+            p3: "Por que você acha que nossos caminhos se cruzaram nesta vida?"
+            r3: "."
+        },
+        {
+        
+            p4: "Qual é a maior dor que você já sentiu? (Não vale dor física)."
+            r4: "."
+        },
+        {
+        
+            p5: "Até agora, qual tem sido a melhor lembrança do ano?"
+            r5: "."
+        },
+        {
+        
+            p6: "Você pretende seguir a carreira que pretende cursar?"
+            r6: "."
+        },
+        {
+        
+            p7: "O que mudou em você depois que nos conhecemos?"
+            r7: "Com a Nicole, "
+        },
+        {
+            p8: "Qual é a coisa que você sempre quis fazer, mas sempre teve muito medo de tentar?"
+            r8: "."
+        },
+        
+    ],
+    }
+
+let entrevista1 = {
+    entrevistador: "Beatris",
+    entrevistado: "Yohana",
+    questoes: [
+        {
+        
+            p1: "Se você tivesse uma oportunidade de aconselhar sobre o futuro, para sua criança interior o que seria?"
+            r1: "."
+        },
+        {
+            p2: "Você acha que a imagem que você tem sobre você é a mesma que as outras pessoas tem?"
+            r2: "." 
+        },
+        {
+            p3: "Por que você acha que nossos caminhos se cruzaram nesta vida?"
+            r3: "."
+        },
+        {
+        
+            p4: "Qual é a maior dor que você já sentiu? (Não vale dor física)."
+            r4: "."
+        },
+        {
+        
+            p5: "Até agora, qual tem sido a melhor lembrança do ano?"
+            r5: "."
+        },
+        {
+        
+            p6: "Você pretende seguir a carreira que pretende cursar?"
+            r6: "."
+        },
+        {
+        
+            p7: "O que mudou em você depois que nos conhecemos?"
+            r7: "Com a Nicole, "
+        },
+        {
+            p8: "Qual é a coisa que você sempre quis fazer, mas sempre teve muito medo de tentar?"
+            r8: "."
+        },
+        
+    ],
+    }
+
+let entrevista1 = {
+    entrevistador: "Nicole",
+    entrevistado: "Beatris",
+    questoes: [
+        {
+        
+            p1: "Se você tivesse uma oportunidade de aconselhar sobre o futuro, para sua criança interior o que seria?"
+            r1: "."
+        },
+        {
+            p2: "Você acha que a imagem que você tem sobre você é a mesma que as outras pessoas tem?"
+            r2: "." 
+        },
+        {
+            p3: "Por que você acha que nossos caminhos se cruzaram nesta vida?"
+            r3: "."
+        },
+        {
+            p4: "Qual é a maior dor que você já sentiu? (Não vale dor física)."
+            r4: "."
+        },
+        {
+        
+            p5: "Até agora, qual tem sido a melhor lembrança do ano?"
+            r5: "."
+        },
+        {
+        
+            p6: "Você pretende seguir a carreira que pretende cursar?"
+            r6: "."
+        },
+        {
+        
+            p7: "O que mudou em você depois que nos conhecemos?"
+            r7: "Com a Beatris, "
+        },
+        {
+            p8: "Qual é a coisa que você sempre quis fazer, mas sempre teve muito medo de tentar?"
+            r8: "."
+        },
+        
+    ],
+    }
+
+let entrevista1 = {
+    entrevistador: "Nicole",
+    entrevistado: "Yohana",
+    questoes: [
+        {
+        
+            p1: "Se você tivesse uma oportunidade de aconselhar sobre o futuro, para sua criança interior o que seria?"
+            r1: "."
+        },
+        {
+            p2: "Você acha que a imagem que você tem sobre você é a mesma que as outras pessoas tem?"
+            r2: "." 
+        },
+        {
+            p3: "Por que você acha que nossos caminhos se cruzaram nesta vida?"
+            r3: "."
+        },
+        {
+        
+            p4: "Qual é a maior dor que você já sentiu? (Não vale dor física)."
+            r4: "."
+        },
+        {
+        
+            p5: "Até agora, qual tem sido a melhor lembrança do ano?"
+            r5: "."
+        },
+        {
+        
+            p6: "Você pretende seguir a carreira que pretende cursar?"
+            r6: "."
+        },
+        {
+        
+            p7: "O que mudou em você depois que nos conhecemos?"
+            r7: "Com a Yohana, "
+        },
+        {
+            p8: "Qual é a coisa que você sempre quis fazer, mas sempre teve muito medo de tentar?"
+            r8: "."
+        },
+        
+    ],
+    }
+
+let entrevista1 = {
+    entrevistador: "Yohana",
+    entrevistado: "Beatris",
+    questoes: [
+        {
+        
+            p1: "Se você tivesse uma oportunidade de aconselhar sobre o futuro, para sua criança interior o que seria?"
+            r1: ""
+        },
+        {
+            p2: "Você acha que a imagem que você tem sobre você é a mesma que as outras pessoas tem?"
+            r2: "Absolutamente não!" 
+        },
+        {
+            p3: "Por que você acha que nossos caminhos se cruzaram nesta vida?"
+            r3: "Porque porquoi"
+        },
+        {
+        
+            p4: "Qual é a maior dor que você já sentiu? (Não vale dor física)."
+            r4: "Quando meus pais se mudaram para outro país."
+        },
+        {
+        
+            p5: "Até agora, qual tem sido a melhor lembrança do ano?"
+            r5: ""
+        },
+        {
+        
+            p6: "Você pretende seguir a carreira que pretende cursar?"
+            r6: "Sim."
+        },
+        {
+        
+            p7: "O que mudou em você depois que nos conhecemos?"
+            r7: "Com a Nicole, "
+        },
+        {
+            p8: "Qual é a coisa que você sempre quis fazer, mas sempre teve muito medo de tentar?"
+            r8: "Pular de paraquedas."
+        },
+        
+    ],
+    }
+
+    let entrevista1 = {
+        entrevistador: "Yohana",
+        entrevistado: "Nicole",
+        questoes: [
+            {
+            
+                p1: "Se você tivesse uma oportunidade de aconselhar sobre o futuro, para sua criança interior o que seria?"
+                r1: "."
+            },
+            {
+                p2: "Você acha que a imagem que você tem sobre você é a mesma que as outras pessoas tem?"
+                r2: "." 
+            },
+            {
+                p3: "Por que você acha que nossos caminhos se cruzaram nesta vida?"
+                r3: "."
+            },
+            {
+                p4: "Qual é a maior dor que você já sentiu? (Não vale dor física)."
+                r4: "."
+            },
+            {
+                p5: "Até agora, qual tem sido a melhor lembrança do ano?"
+                r5: "."
+            },
+            {
+                p6: "Você pretende seguir a carreira que pretende cursar?"
+                r6: "."
+            },
+            {
+                p7: "O que mudou em você depois que nos conhecemos?"
+                r7: "Com Beatris, ."
+            },
+            {
+                p8: "Qual é a coisa que você sempre quis fazer, mas sempre teve muito medo de tentar?"
+                r8: "."
+            },
+            
+        ],
+        }
